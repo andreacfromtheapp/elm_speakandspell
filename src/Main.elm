22fb81201b1c5dfe6818c589f2e3ca0433b19049
gacallea <gacallea@users.noreply.github.com>
2022-04-23 17:53:32 +0200

    revert build per fabius

--- src/Main.elm
+++ src/Main.elm
@@ -37,6 +37,7 @@ randomWordsApiUrl =
 
 type Msg
     = OnResize Int Int
+    | DoNothing
     | GetNewWord (Result Http.Error (List NewWord))
     | KeyPressed KeyboardEvent
     | KeyClicked String
@@ -810,6 +811,11 @@ update msg model =
     case msg of
         OnResize x y ->
             ( { model | viewport = { width = x, height = y } }
+            , Cmd.none
+            )
+
+        DoNothing ->
+            ( model
             , Cmd.none
             )
 
