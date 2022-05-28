5761680f11e1a022f18a2af93f93c90f6681fe69
gacallea <gacallea@users.noreply.github.com>
2022-05-28 14:34:10 +0200

    mv to vite, ready for css next?

---
+++ src/js/main.js
@@ -0,0 +1,39 @@
+import '../css/styles.css'
+import { Elm } from '../elm/Main.elm'
+
+const app = Elm.Main.init({
+  // get window size with flags
+  flags: {
+    win_width: window.innerWidth,
+    win_height: window.innerHeight
+  },
+
+  // Start the Elm application.
+  node: document.querySelector('main')
+})
+
+// Instantiate Speech Synth API
+const synth = window.speechSynthesis
+const utter = new SpeechSynthesisUtterance()
+
+// Pause/Resume Speech Synth API (SetSound On | Off)
+app.ports.sound.subscribe(function (message) {
+  synth.cancel()
+  if (message === true) {
+    synth.resume()
+  } else {
+    synth.pause()
+  }
+})
+
+// We receive the whole word here and speak it
+app.ports.speak.subscribe(function (message) {
+  utter.text = message
+  synth.speak(utter)
+})
+
+// We receive the split word here and spell it
+app.ports.spell.subscribe(function (message) {
+  utter.text = message
+  synth.speak(utter)
+})
---
+++ vite.config.js
@@ -0,0 +1,42 @@
+import { defineConfig } from 'vite'
+import elmPlugin from 'vite-plugin-elm'
+import { ViteWebfontDownload } from 'vite-plugin-webfont-dl'
+import viteImagemin from 'vite-plugin-imagemin'
+import viteCompression from 'vite-plugin-compression'
+import ViteCspPlugin from 'vite-plugin-csp'
+
+export default defineConfig({
+  plugins: [
+    elmPlugin(),
+    ViteCspPlugin(),
+    ViteWebfontDownload(),
+    viteImagemin({
+      gifsicle: {
+        optimizationLevel: 3,
+        interlaced: false
+      },
+      optipng: {
+        optimizationLevel: 7
+      },
+      mozjpeg: {
+        quality: 80,
+        progressive: true
+      },
+      pngquant: {
+        quality: [0.8, 0.9],
+        speed: 10
+      },
+      svgo: {
+        plugins: ['preset-default']
+      },
+      webp: {
+        quality: 80,
+        method: 4,
+        nearLossless: 95
+      }
+    }),
+    viteCompression({
+      algorithm: 'brotliCompress'
+    })
+  ]
+})
