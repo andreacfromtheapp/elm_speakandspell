1da29313becd6183ff4698bca5dcde9af676927b
gacallea <gacallea@users.noreply.github.com>
2022-05-30 13:32:14 +0200

    no more flags needed with tailwind

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,7 @@
-import '../css/styles.css'
+import '../css/style.css'
 import { Elm } from '../elm/Main.elm'
 
 const app = Elm.Main.init({
-  // get window size with flags
-  flags: {
-    win_width: window.innerWidth,
-    win_height: window.innerHeight
-  },
-
   // Start the Elm application.
   node: document.querySelector('main')
 })
